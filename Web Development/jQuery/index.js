ef4d68cc7727398347bd30917c4f1cbc3abb20b3
Vivek <viveknikate.2000@gmail.com>
2023-04-12 12:43:41 +0530

    JQuery Completed..! Frontend completed nearly.

---
+++ Web Development/jQuery/index.js
@@ -0,0 +1,15 @@
+
+// document.querySelector ===  jQuery === $ 
+
+jQuery("h1").css('color','red');        
+
+$('h1').css('font-family','arial');
+
+$(document).ready(function (){
+     $('h1').css('color','deeppink');
+});
+
+$('h1').addClass('big-title');
+
+
+
