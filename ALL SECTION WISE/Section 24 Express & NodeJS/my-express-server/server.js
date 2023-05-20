8d723888d4739534c331d9c8d1f2e2c3a8c2fa43
Vivek <viveknikate.2000@gmail.com>
2023-05-20 23:06:04 +0530

    Learning Express Js & Node Js & Git Notes updated

---
+++ ALL SECTION WISE/Section 24 Express & NodeJS/my-express-server/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+     // console.log(req);
+     console.log("This is Home page");
+     res.send('<h1> Hello world..!</h1>')
+})
+
+app.get('/contact', function (req, res){
+     console.log("This is Contact page");
+     res.send("<h1>ContactUs on : vivek.nikate@capgemini.com</h1>")
+})
+
+app.get('/about', function (req, res){
+     console.log('This is about us page')
+     res.send("<center><h1>This is My WebSite. Software Engineer at Capgemini</h1></center>")
+})
+
+app.listen(4555,()=>{
+     console.log("On Port no. 4555 server is running");
+});
